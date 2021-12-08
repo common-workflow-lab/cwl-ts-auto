6a9c8d3443390f91c74a2027e3c49fcf8d74c408
Adrian Zimmer <z.adrian1995@gmail.com>
2021-12-08 13:36:01 +0100

    Add documentation for SimpleSchema

--- src/simpleSchema.ts
+++ src/simpleSchema.ts
@@ -11,23 +11,49 @@ import {
 import { v4 as uuidv4 } from 'uuid'
 import * as Internal from './util/internal'
 
+
+/**
+ * Auto-generated class implementation for SimpleSchema
+ *
+ * This is a test doc that
+ * spans multiple lines.
+ * 
+ * Hello doc!
+ * 
+ */
 export class SimpleSchema extends Saveable {
   loadingOptions: LoadingOptions
   extensionFields?: Dictionary<any>
+
+  /**
+   * Test doc
+   */
   label: undefined | string
 
-  constructor ({ extensionFields, loadingOptions, label }: {extensionFields?: Dictionary<any>, loadingOptions?: LoadingOptions, label: undefined | string }) {
+
+  constructor ({extensionFields, loadingOptions, label}: {extensionFields?: Dictionary<any>, loadingOptions?: LoadingOptions,  label: undefined | string}) {
     super()
     this.extensionFields = extensionFields ?? {}
     this.loadingOptions = loadingOptions ?? new LoadingOptions({})
     this.label = label
   }
 
+  /**
+   * Used to construct instances of {@link SimpleSchema }.
+   *
+   * @param __doc                           Document fragment to load this record object from.
+   * @param baseuri                         Base URI to generate child document IDs against.
+   * @param loadingOptions                  Context for loading URIs and populating objects.
+   * @param docRoot                         ID at this position in the document (if available)
+   * @returns                               An instance of {@link SimpleSchema }
+   * @throws {@link ValidationException}    If the document fragment is not a
+   *                                        {@link Dictionary} or validation of fields fails.
+   */
   static override async fromDoc (__doc: any, baseuri: string, loadingOptions: LoadingOptions,
     docRoot?: string): Promise<Saveable> {
     const _doc = Object.assign({}, __doc)
     const errors: ValidationException[] = []
-
+            
     let label
     try {
       label = await loadField(_doc.label, LoaderInstances.unionOfundefinedtypeOrstrtype,
@@ -67,6 +93,6 @@ export class SimpleSchema extends Saveable {
     })
     return schema
   }
-
+        
   static attr: Set<string> = new Set(['label'])
 }
